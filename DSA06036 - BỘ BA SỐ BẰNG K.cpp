6b269a9ed5467f7eb7566f0e6138004847d53139
nguyenvantu11052002 <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-03-01 09:16:35 +0700

    Create DSA06036 - BỘ BA SỐ BẰNG K.cpp

---
+++ DSA06036 - BỘ BA SỐ BẰNG K.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+
+bool tim(int a[], int n, int x, int l, int r){
+	while(l <= r){
+		int m = (l + r) / 2;
+		if(a[m] == x){
+			return true;
+		}
+		else if(x > a[m]){
+			l = m + 1;
+		}
+		else r = m - 1;
+	}
+	return false;
+}
+
+int check(int a, int b){
+	long long cmu2 = (long long)a * a + (long long) b * b;
+	int c = sqrt(cmu2);
+	if((long long)c * c == cmu2) return c;
+	return 0;
+}
+	
+void inraketqua(){
+	int n, k;
+	cin >> n >> k;
+	int a[n];
+	for(int i = 0; i < n; i++){
+		cin >> a[i];
+	}
+	sort(a, a + n);
+	for(int i = 0; i < n - 1; i++){
+		for(int j = i + 1; j < n; j++){
+			int x = k - a[i] - a[j];
+			if(tim(a, n, x, j + 1, n - 1)){
+				cout << "YES" << endl;
+				return;
+			}
+		}
+	}
+	cout << "NO" << endl;
+}
+ 
+int main(){
+	int t = 1;
+	cin >> t;
+	while(t--){
+		inraketqua();
+	}
+	return 0;
+}
