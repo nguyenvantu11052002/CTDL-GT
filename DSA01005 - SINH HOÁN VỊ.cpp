903e1e84a788957294734b74523ac3a95301b1fe
nguyenvantu11052002 <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-03-01 08:50:33 +0700

    Create DSA01005 - SINH HOÁN VỊ.cpp

---
+++ DSA01005 - SINH HOÁN VỊ.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+
+int ok, a[25], n, k;
+
+void ktao(){
+	for(int i = 1; i <= n; i++){
+		a[i] = i;
+	}
+}
+
+void sinh(){
+	int i = n - 1;
+	while(i >= 1 && a[i] > a[i + 1]){
+		i--;
+	}
+	if(i == 0){
+		ok = 0;
+	}
+	else{
+		int j = n;
+		while(a[i] > a[j]) j--;
+		swap(a[i], a[j]);
+		reverse(a + i + 1, a + n + 1);
+	}
+}
+
+void inraketqua(){
+	cin >> n;
+	ktao();
+	ok = 1;
+	while(ok){
+		for(int i = 1; i <= n; i++){
+			cout << a[i];
+		}
+		cout << " ";
+		sinh();
+	}
+	cout << endl;
+}
+ 
+int main(){
+	int t = 1;
+	cin >> t;
+	while(t--){
+		inraketqua();
+	}
+	return 0;
+}
