f1621f48fce87639c0f3fa39f8d1a8341d667799
nguyenvantu11052002 <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-03-18 21:41:15 +0700

    Create DSAKT110 - NHÀ KHÔNG KỀ NHAU.cpp

---
+++ DSAKT110 - NHÀ KHÔNG KỀ NHAU.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+
+
+
+void TC(){
+	int n;
+	cin >> n;
+	int a[n];
+	for(int i = 0; i < n; i++){
+		cin >> a[i];
+	}
+	long long dp[n];
+	dp[0] = a[0];
+	dp[1] = max(a[0], a[1]);
+	for(int i = 2; i < n; i++){
+		dp[i] = max(dp[i - 2] + a[i], dp[i - 1]);
+	}
+	cout << dp[n - 1] << endl;
+}		
+ 
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	int t = 1;
+	cin >> t;
+	while(t--){
+		TC();
+	}
+	return 0;
+}
+	
+
+
+
