1bab005b719ba7e092db1b305f04221099f007c3
nguyenvantu11052002 <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-04-30 22:38:56 +0700

    Create DSA07012 - BIẾN ĐỔI HẬU TỐ - TRUNG TỐ.cpp

---
+++ DSA07012 - BIẾN ĐỔI HẬU TỐ - TRUNG TỐ.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+bool check (char c) {
+	if(c == '+' || c == '-' || c == '*' || c == '/') return true;
+	return false;
+}
+
+void TC() {
+	string s;
+	cin >> s;
+	int n = s.size();
+	stack<string> st;
+	for (int i = 0; i < n; i++) {
+		if (check(s[i])) {
+			string s1 = st.top();
+			st.pop();
+			string s2 = st.top();
+			st.pop();
+			string them = "(" + s2 + s[i] + s1 + ")";
+			st.push(them);
+		}
+		else{
+			st.push(string(1, s[i]));
+		}
+	}
+	cout << st.top() << endl;
+}
+
+int main() {	
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		TC();
+	}
+	return 0;
+}
