f7e6282595d1828b43b4b743cac96d4410d45b75
nguyenvantu11052002 <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-05-02 22:39:58 +0700

    Create DSA07008 - BIẾN ĐỔI TRUNG TỐ - HẬU TỐ.cpp

---
+++ DSA07008 - BIẾN ĐỔI TRUNG TỐ - HẬU TỐ.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int ut (char c) {
+	if (c == '^') return 3;
+	if (c == '/' || c == '*') return 2;
+	if (c == '+' || c == '-') return 1;
+	return 0;
+}
+
+void TC() {
+	string s;
+	cin >> s;
+	int n = s.size();
+	stack<char> st; // luu cac dau + - * / ^ va (
+	string res = ""; // luu cac ki tu A B C D ... 
+	for (int i = 0; i < n; i++) {
+		if (s[i] == '(') st.push(s[i]);
+		else if (s[i] == ')'){
+			while (st.top() != '(') {
+				res += st.top();
+				st.pop();
+			}
+			st.pop();
+		}
+		else if (s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/' || s[i] == '^') {
+			while (!st.empty() && ut (st.top()) >= ut (s[i])) {
+				res += st.top();
+				st.pop();
+			}
+			st.push(s[i]);
+		}
+		else res += s[i];
+	}
+	while (!st.empty()) {
+		if (st.top() != '(') {
+			res += st.top();
+		}
+		st.pop();
+	}
+	cout << res << endl;
+}
+
+int main() {	
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		TC();
+	}
+	return 0;
+}
