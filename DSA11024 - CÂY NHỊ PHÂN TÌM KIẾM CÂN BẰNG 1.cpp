4f5264f52e5e36caf5cf785e2b457da2b9d936f9
Nguyễn Văn Tú <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-05-30 17:58:21 +0700

    Create DSA11024 - CÂY NHỊ PHÂN TÌM KIẾM CÂN BẰNG 1.cpp

---
+++ DSA11024 - CÂY NHỊ PHÂN TÌM KIẾM CÂN BẰNG 1.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+
+void TC(){
+	int n;
+	cin >> n;
+	int a[n];
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	sort(a, a + n);
+	cout << a[(n - 1)/2] << endl;
+}
+ 
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	int t = 1;
+	cin >> t;
+	while(t--){
+		TC();
+	}
+	return 0;
+}
