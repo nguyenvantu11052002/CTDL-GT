52ad082525255927b10d433a0efa925fbf55ca9e
Nguyễn Văn Tú <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-06-14 11:02:17 +0700

    Create DSA11022 - NODE TRUNG GIAN CỦA CÂY NHỊ PHÂN TÌM KIẾM Nguyễn Thị Thêu.cpp

---
+++ DSA11022 - NODE TRUNG GIAN CỦA CÂY NHỊ PHÂN TÌM KIẾM Nguyễn Thị Thêu.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+vector<int> res;
+
+struct node{
+	int data;
+	node *l;
+	node *r;
+	node (int x) {
+		data = x;
+		l = NULL;
+		r = NULL;
+	}
+};
+
+void themnode (node* &t, int x) {
+	if (t == NULL) t = new node(x);
+	else if (x < t->data ) {
+		themnode(t -> l, x);
+	}
+	else themnode(t -> r, x);
+}
+
+void LNR (node* t) {
+	if (t == NULL) return;
+	LNR(t -> l);
+	if (t->l == NULL && t->r == NULL) res.push_back(t->data);
+	LNR(t -> r);
+}
+
+void TC () {
+	res.clear();
+	int n, x;
+	cin >> n;
+	int m = n;
+	node *t = NULL;
+	while (n--) {
+		cin >> x;
+		themnode(t, x); // tao cay nhi phan tim kiem;
+	}
+	LNR(t);
+	cout << m - res.size() << endl;
+}
+
+int main () {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	
+	int T = 1;
+	cin >> T;
+	while (T--) {
+		TC();
+	}
+	return 0;
+}
